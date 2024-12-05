0db7db516fd8151370f11b4523b9a2ef07e773d2
Анатольев Алексей <anatolyev@yandexlyceum.ru>
2024-12-05 16:12:33 +0300

    Создали цикл с ожиданием!

--- 1205_PyGame1/main.py
+++ 1205_PyGame1/main.py
@@ -6,3 +6,9 @@ if __name__ == '__main__':
     pygame.init()
     size = width, height = 800, 600
     screen = pygame.display.set_mode(size)
+
+    # Смена кадров (обновление экрана)
+    pygame.display.flip()
+
+    while pygame.event.wait().type != pygame.QUIT:
+        pass
