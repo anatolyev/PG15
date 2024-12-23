9ea9f0c51728c522f1fe4e2a37f8aba023a6b81f
Анатольев Алексей <anatolyev@yandexlyceum.ru>
2024-12-23 17:11:20 +0300

    рабочие горы и столкновения

--- 1223_PyGame6/main2.py
+++ 1223_PyGame6/main2.py
@@ -9,41 +9,34 @@ HORIZONTAL_BORDERS = pygame.sprite.Group()
 VERTICAL_BORDERS = pygame.sprite.Group()
 
 
-def load_image(name, colorkey=None):
+def load_image(name):
     fullname = os.path.join('images', name)
     if not os.path.isfile(fullname):
         print(f'Файл с изображением "{fullname}" не найден')
         sys.exit()
     image = pygame.image.load(fullname)
-    if colorkey is not None:
-        image = image.convert()  # оптимизирует формат изображения и ускоряет отрисовку
-        if colorkey == -1:
-            colorkey = image.get_at((0, 0))
-        image.set_colorkey(colorkey)
-    else:
-        image = image.convert_alpha()  # спользуется для добавления прозрачности к изображению
     return image
 
 
-
 class Mountain(pygame.sprite.Sprite):
-    image = load_image("mountains.png", -1)
+    image = load_image("mountains.png")
 
-    def __init__(self, size):
+    def __init__(self, height):
         super().__init__(ALL_SPRITES)
         self.image = Mountain.image
         self.rect = self.image.get_rect()
         # вычисляем маску для эффективного сравнения
         self.mask = pygame.mask.from_surface(self.image)
         # располагаем горы внизу
-        self.rect.bottom = size[0]
+        self.rect.bottom = height
 
 
 class Landing(pygame.sprite.Sprite):
-    image = load_image("pt.png", -1)
+    image = load_image("pt.png")
 
-    def __init__(self, pos):
+    def __init__(self, pos, mountain):
         super().__init__(ALL_SPRITES)
+        self.mountain = mountain
         self.image = Landing.image
         self.rect = self.image.get_rect()
         # вычисляем маску для эффективного сравнения
@@ -52,34 +45,34 @@ class Landing(pygame.sprite.Sprite):
         self.rect.y = pos[1]
 
     def update(self):
-        self.rect = self.rect.move(0, 1)
+        # если ещё в небе
+        if not pygame.sprite.collide_mask(self, self.mountain):
+            self.rect = self.rect.move(0, 1)
 
 
 def main():
     # pygame setup
     pygame.init()
-    size = width, height = 1280, 720
+    size = width, height = 700, 700
     screen = pygame.display.set_mode(size)
-
-    mountain = Mountain(size)
+    mountain = Mountain(height)
 
     clock = pygame.time.Clock()
     # fill the screen with a color to wipe away anything from last frame
 
-    # images = load_image('bomb.png')
+    # image = load_image('bomb.png')
     running = True
 
     while running:
-
+        screen.fill("dark blue")
         # poll for events
         # pygame.QUIT event means the user clicked X to close your window
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
             if event.type == pygame.MOUSEBUTTONDOWN:
-                Landing(event.pos)
+                Landing(event.pos, mountain)
         # RENDER YOUR GAME HERE
-        screen.fill(pygame.Color("black"))
         ALL_SPRITES.draw(screen)
         ALL_SPRITES.update()
 
