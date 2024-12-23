dd64ac0d1992f7fbe0dcc7093f902e4f0a85bdc9
Анатольев Алексей <anatolyev@yandexlyceum.ru>
2024-12-23 16:41:21 +0300

    добавили классы шарика и границ и создали их

--- 1223_PyGame6/main.py
+++ 1223_PyGame6/main.py
@@ -2,23 +2,71 @@
 import os
 import sys
 import pygame
+import random
 
-def load_image(name):
-    fullname = os.path.join('images', name)
-    if not os.path.isfile(fullname):
-        print(f'Файл с изображением "{fullname}" не найден')
-        sys.exit()
-    image = pygame.image.load(fullname)
-    return image
+ALL_SPRITES = pygame.sprite.Group()
+HORIZONTAL_BORDERS = pygame.sprite.Group()
+VERTICAL_BORDERS = pygame.sprite.Group()
+
+
+# def load_image(name):
+#     fullname = os.path.join('images', name)
+#     if not os.path.isfile(fullname):
+#         print(f'Файл с изображением "{fullname}" не найден')
+#         sys.exit()
+#     image = pygame.image.load(fullname)
+#     return image
+
+class Ball(pygame.sprite.Sprite):
+    def __init__(self, radius, x, y):
+        super().__init__(ALL_SPRITES)
+        self.radius = radius
+        self.image = pygame.Surface((2 * radius, 2 * radius),
+                                    pygame.SRCALPHA, 32)
+        pygame.draw.circle(self.image, pygame.Color("red"),
+                           (radius, radius), radius)
+        self.rect = pygame.Rect(x, y, 2 * radius, 2 * radius)
+        self.vx = random.randint(-5, 5)
+        self.vy = random.randrange(-5, 5)
+
+    def update(self):
+        self.rect = self.rect.move(self.vx, self.vy)
+
+
+class Border(pygame.sprite.Sprite):
+    # строго вертикальный или строго горизонтальный отрезок
+    def __init__(self, x1, y1, x2, y2):
+        super().__init__(ALL_SPRITES)
+        if x1 == x2:  # вертикальная стенка
+            self.add(VERTICAL_BORDERS)
+            self.image = pygame.Surface([1, y2 - y1])
+            self.rect = pygame.Rect(x1, y1, 1, y2 - y1)
+        else:  # горизонтальная стенка
+            self.add(HORIZONTAL_BORDERS)
+            self.image = pygame.Surface([x2 - x1, 1])
+            self.rect = pygame.Rect(x1, y1, x2 - x1, 1)
+
+
 
 def main():
     # pygame setup
     pygame.init()
-    screen = pygame.display.set_mode((1280, 720))
+    size = width, height = 1280, 720
+    screen = pygame.display.set_mode(size)
+
+    Border(5, 5, width - 5, 5)
+    Border(5, height - 5, width - 5, height - 5)
+    Border(5, 5, 5, height - 5)
+    Border(width - 5, 5, width - 5, height - 5)
+
+    for i in range(10):
+        Ball(20, 100, 100)
+
+
     clock = pygame.time.Clock()
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("white")
-    image = load_image('bomb.png')
+    # image = load_image('bomb.png')
     running = True
 
     while running:
@@ -27,8 +75,8 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                screen.blit(image, event.pos)
+            # if event.type == pygame.MOUSEBUTTONDOWN:
+            #     screen.blit(image, event.pos)
         # RENDER YOUR GAME HERE
         # flip() the display to put your work on screen
         pygame.display.flip()
